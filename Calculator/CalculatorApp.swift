294ee430a67b2d921997ef9a49e671fd824866f9
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-07-27 20:36:55 +0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Auto on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
